7225b4259e7aa1e185a01ff575254384083c28eb
Aline Klauck <55554722+Alinesete@users.noreply.github.com>
2022-03-15 18:02:08 -0300

    Room imput F.E

--- app.js
+++ app.js
@@ -130,3 +130,8 @@ app.post("/delete", (req, res) => {
 
 // Listen on randomly assigned cloud port AND local port 3000
 app.listen(process.env.PORT || 3000, () => {console.log("Server started on port 3000")});
+
+// Redirect to room
+function redr(key){
+  alert("Oi");
+}
